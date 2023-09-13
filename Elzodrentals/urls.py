cfee27c92615ea4e766e8f17f4406e1acb2b38e9
Joseph <60448358+joebuggy561@users.noreply.github.com>
2023-09-13 15:26:15 +0400

    fifth commit

--- Elzodrentals/urls.py
+++ Elzodrentals/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -27,4 +28,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('bookings/', include('bookings.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
 
