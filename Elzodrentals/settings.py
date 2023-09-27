0589d716b8725aed4793873a422a84b202c875fd
Joseph <60448358+joebuggy561@users.noreply.github.com>
2023-09-27 16:19:10 +0400

    7th commit

--- Elzodrentals/settings.py
+++ Elzodrentals/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     'car_rentals_page/static',
 ]
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [
 # STATIC_ROOT = BASE_DIR / "staticfiles" # new
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR /'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
